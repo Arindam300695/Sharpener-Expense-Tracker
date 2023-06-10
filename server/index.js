e0bfd1e9855b0efb5c9b937d11f6710654d6ff60
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-06-10 10:39:55 +0530

    fixes sum bugs

--- server/index.js
+++ server/index.js
@@ -18,8 +18,9 @@ const helmet = require("helmet");
 const app = express();
 const morgan = require("morgan");
 const fs = require("fs");
+const path = require("path");
 
-const accessLogStream = fs.createReadStream(
+const accessLogStream = fs.createWriteStream(
 	path.join(__dirname, "access.log"),
 	{ flags: "a" },
 );
