f1b06c79748514eb93b6e8488153efaa406c26d1
Arindam Chattopadhyay <arindam.chattopadhyay300695@gmail.com>
2023-06-10 10:45:53 +0530

    .

--- client/src/pages/PreviousDowloadedReportList.jsx
+++ client/src/pages/PreviousDowloadedReportList.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import axios from "axios";
 import moment from "moment";
 
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://expense-tracker-ri7u.onrender.com";
 
 const PreviousDowloadedReportList = () => {
 	const [downLoadedReportList, setDonwloadedReportList] = useState([]);
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -7,7 +7,7 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://expense-tracker-ri7u.onrender.com";
 
 const Signup = () => {
 	const navigate = useNavigate();
